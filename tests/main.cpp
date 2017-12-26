91e62beaecf4892f9e7740241e2d1442a1037d60
M. Petra Baranski <info@progsource.de>
2017-12-26 11:42:57 +0100

    added travis.yml

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,7 +8,7 @@
 // -----------------------------------------------------------------------------
 
 int main (int argc, char** argv) {
-  ::testing::GTEST_FLAG(throw_on_failure) = false;
+  ::testing::GTEST_FLAG(throw_on_failure) = true;
   ::testing::InitGoogleMock(&argc, argv);
   return RUN_ALL_TESTS();
 }
