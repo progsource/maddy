e8ba8f661af7fe966ac105f6f37358a7a0a22ef4
Patrick José Pereira <patrickelectric@gmail.com>
2018-10-19 11:50:04 -0300

    test_maddy_italicparser: Add tests for the italic parser
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ tests/maddy/test_maddy_italicparser.cpp
@@ -0,0 +1,23 @@
+/*
+ * This project is licensed under the MIT license. For more information see the
+ * LICENSE file.
+ */
+#include <memory>
+
+#include "gmock/gmock.h"
+
+#include "maddy/italicparser.h"
+
+// -----------------------------------------------------------------------------
+
+TEST(MADDY_ITALICPARSER, ItReplacesMarkdownWithItalicHTML)
+{
+
+  std::string text = "some text *bla* text testing *it* out";
+  std::string expected = "some text <i>bla</i> text testing <i>it</i> out";
+  auto italicParser = std::make_shared<maddy::ItalicParser>();
+
+  italicParser->Parse(text);
+
+  ASSERT_EQ(text, expected);
+}
