a84da9de61d1428073cc6e721433d3ee8aa7e976
Patrick José Pereira <patrickelectric@gmail.com>
2018-10-25 14:47:52 -0300

    emphasizedparser: Underscore does not need backslash
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- include/maddy/emphasizedparser.h
+++ include/maddy/emphasizedparser.h
@@ -41,7 +41,7 @@ public:
   void
   Parse(std::string& line) override
   {
-    static std::regex re("(?!.*`.*|.*<code>.*)\\_(?!.*`.*|.*<\\/code>.*)([^\\_]*)\\_(?!.*`.*|.*<\\/code>.*)");
+    static std::regex re("(?!.*`.*|.*<code>.*)_(?!.*`.*|.*<\\/code>.*)([^_]*)_(?!.*`.*|.*<\\/code>.*)");
     static std::string replacement = "<em>$1</em>";
 
     line = std::regex_replace(line, re, replacement);
